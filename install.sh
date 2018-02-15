9e091174e0f3ee59f5fcb978d36bbcf3db77144a
LocalGh0st <36140262+LocalGh0stMZ@users.noreply.github.com>
2018-02-15 01:06:49 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#
+#  ╔╦╗╔═╗╦  ╔═╗╔═╗╦═╗╔═╗╔╦╗
+#   ║ ║╣ ║  ║╣ ║ ╦╠╦╝╠═╣║║║
+#   ╩ ╚═╝╩═╝╚═╝╚═╝╩╚═╩ ╩╩ ╩
+#   Canal:  @AnonymousMz
+#   Grupo:  @NHAnonymousMz
+#   Acervo: @AnonyMz_Bot
+#
+#   ╔╗ ╦ ╦
+#   ╠╩╗╚╦╝
+#   ╚═╝ ╩
+#   ╦  ╔═╗╔═╗╔═╗╦  ╔═╗╦ ╦╔═╗╔═╗╔╦╗
+#   ║  ║ ║║  ╠═╣║  ║ ╦╠═╣║ ║╚═╗ ║
+#   ╩═╝╚═╝╚═╝╩ ╩╩═╝╚═╝╩ ╩╚═╝╚═╝ ╩
+#
+#    LocalGhost :  @LocalGh0st
+#
+################################################
+apt update
+apt upgrade -y
+# Open Folder
+cd $HOME
+cd banner
+# Edit Bash.bashrc File
+cat "logo.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
